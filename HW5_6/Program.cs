bb7e88848ac29633a4357c06b6ac94ca8b8d5ac8
Saitama267 <85219557+Saitama267@users.noreply.github.com>
2023-05-04 14:15:15 +0200

    Add project files.

---
+++ HW5_6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace HW5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 0;
+            while (true)
+            {
+                Console.WriteLine("\tMenu");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("1- ");
+                Console.WriteLine("2- ");
+                Console.WriteLine("3- ");
+                Console.WriteLine("4- ");
+                Console.WriteLine("0- ");
+                Int32.TryParse(Console.ReadLine(), out menu);
+                if (menu == 0)
+                {
+                    break;
+                }
+                else if (menu == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter text with digits");
+                    
+                }
+                else if (menu == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter text with digits");
+                   
+                }
+                else if (menu == 3)
+                {
+                    Console.Clear();
+                   
+                }
+                else if (menu == 4)
+                {
+                    Console.Clear();
+                   
+                }
+            }
+
+        }
+    }
+}
