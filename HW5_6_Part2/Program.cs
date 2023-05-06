f4066f8f1f91f0d814cd0a9102001cc565a75d0e
Saitama267 <85219557+Saitama267@users.noreply.github.com>
2023-05-06 15:28:37 +0200

    Add new project

---
+++ HW5_6_Part2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5_6_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
